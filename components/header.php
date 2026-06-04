f5757d1eb7907c58c7b8a57ea6f210d0edb4c34b
Bhusan-Shrestha <bhushanshrestha2060@gmail.com>
2026-06-04 12:50:08 +0545

    Fixed Header

--- components/header.php
+++ components/header.php
@@ -1,14 +1,18 @@
 <?php $currentPage = basename($_SERVER['PHP_SELF']); ?>
 <header>
     <img src="images/hamro-restaurant-logo.png" alt="Hamro Restaurant Logo" class="logo" style="width: 40px; height: 40px;">
-    <h1>Hamro Restaurant</h1>
+    <div style="text-align:center; align-content: center; justify-content: center;">
+        <h1>Hamro Restaurant</h1>
+    </div>
     <nav>
-            <ul class="nav-links">
-                <li><a href="index.php" class="<?php echo $currentPage === 'index.php' ? 'active' : ''; ?>">Home</a></li>
-                <li><a href="about.php" class="<?php echo $currentPage === 'about.php' ? 'active' : ''; ?>">About Us</a></li>
-                <li><a href="menus.php" class="<?php echo $currentPage === 'menus.php' ? 'active' : ''; ?>">Menus</a></li>
-                <li><a href="reserve.php" class="<?php echo $currentPage === 'reserve.php' ? 'active' : ''; ?>">Reserve Now</a></li>
-            </ul>
-        </nav>
+        <ul class="nav-links">
+            <li><a href="index.php" class="<?php echo $currentPage === 'index.php' ? 'active' : ''; ?>">Home</a></li>
+            <li><a href="about.php" class="<?php echo $currentPage === 'about.php' ? 'active' : ''; ?>">About Us</a>
+            </li>
+            <li><a href="menus.php" class="<?php echo $currentPage === 'menus.php' ? 'active' : ''; ?>">Menus</a></li>
+            <li><a href="reserve.php" class="<?php echo $currentPage === 'reserve.php' ? 'active' : ''; ?>">Reserve
+                    Now</a></li>
+        </ul>
+    </nav>
     <div class="menu-toggle" onclick="toggleMenu()">&#9776;</div>
-    </header>
+</header>
