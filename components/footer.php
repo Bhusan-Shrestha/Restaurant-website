55f4b0a3e0e5cbacfe21458ed1defefab6986410
Bhusan-Shrestha <bhushanshrestha2060@gmail.com>
2026-06-04 13:33:46 +0545

    Added Automatic year copyright

--- components/footer.php
+++ components/footer.php
@@ -68,7 +68,7 @@
     </div>
 
     <div class="site-footer__bottom">
-        <p>&copy; Copyright 2026. All Rights Reserved, Hamro Restaurant.</p>
+        <p>&copy; Copyright 2026 - <?php echo date("Y"); ?>. All Rights Reserved, Hamro Restaurant.</p>
         <p>Designed by<a href="https://bhusanshrestha.com.np/" target="_blank" style="text-decoration:none; color:#fc8c05;"> Bhusan Shrestha</a></p>
     </div>
 </footer>
